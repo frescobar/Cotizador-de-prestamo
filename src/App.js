82199871e3c20845aad43260e640b459f95fd75c
frescobar <frescobar87@gmail.com>
2020-12-17 14:31:47 -0500

    Added names

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React, {useState} from 'react';
-import "./index.css"
-import Header from './Components/Header'
-import Form from './Components/Form';
-
+import React, { useState } from "react";
+import "./index.css";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
 
 function App() {
-
   // state de la cantidad del Formulario
-  let [cantidad,setCantidad] = useState(0)
+  let [cantidad, setCantidad] = useState(0);
 
-  let [plazo, setPlazo] = useState("")
+  let [plazo, setPlazo] = useState("");
 
+  const handleCantidad = (e) => {
+    setCantidad(parseFloat((cantidad = e.target.value)));
+  };
 
-    const handleCantidad = e =>{
-        setCantidad(parseFloat(cantidad = e.target.value))
-    }
-
-    const handlePlazo = e => {
-      setPlazo( plazo = parseInt(e.target.value))
-    }
+  const handlePlazo = (e) => {
+    setPlazo((plazo = parseInt(e.target.value)));
+  };
   return (
     <div>
-      <Header 
-      titulo = "Cotizador de Préstamos"
-      descripcion = "Llena los Campos en el formulario y obten una cotización al instante"
+      <Header
+        titulo='Cotizador de Préstamos'
+        descripcion='Llena los Campos en el formulario y obten una cotización al instante'
       />
       <Form
-      cantidad ={cantidad}
-      handleCantidad = {handleCantidad}
-      plazo={plazo}
-      handlePlazo ={handlePlazo}
+        cantidad={cantidad}
+        handleCantidad={handleCantidad}
+        plazo={plazo}
+        handlePlazo={handlePlazo}
       />
+      <p>Por Freddys Escobar</p>
+      <p>Profesor:Roberto Elias</p>
     </div>
   );
 }
